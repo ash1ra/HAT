d47984ab6136d77439fb89e2b15cd4cda821c97d
ash1ra <ash1rawtf@gmail.com>
2026-02-17 12:05:54 +0200

    feat: create `create_logger` function

--- utils.py
+++ utils.py
@@ -1,5 +1,50 @@
+import logging
+import sys
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
 from einops import rearrange
 from torch import Tensor
+
+
+def create_logger(
+    log_level: str,
+    log_file_name: str,
+    max_log_file_size: int = 5 * 1024 * 1024,
+    backup_count: int = 10,
+) -> logging.Logger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%d.%m.%Y %H:%M:%S",
+    )
+
+    logger.handlers.clear()
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(getattr(logging, log_level.upper()))
+    logger.addHandler(console_handler)
+
+    Path("logs").mkdir(parents=True, exist_ok=True)
+    current_date = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+    log_file_name = f"logs/{log_file_name}_{current_date}.log"
+
+    file_handler = RotatingFileHandler(
+        filename=log_file_name,
+        maxBytes=max_log_file_size,
+        backupCount=backup_count,
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = create_logger(log_level="INFO", log_file_name="HAT")
 
 
 def split_img_into_windows(img_tensor: Tensor, window_size: int) -> Tensor:
